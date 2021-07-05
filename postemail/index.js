48f0872e5e4326865a67da3867b8ba36b3ec7712
姜旗 <1292692959@qq.com>
2021-07-05 13:25:08 +0800

    add 8 hour

--- postemail/index.js
+++ postemail/index.js
@@ -1,6 +1,5 @@
 const nodemailer = require('nodemailer'); //发送邮件的node插件
 const dayjs = require('dayjs');
-const fetch = require('node-fetch');
 const template = require('art-template');
 const inlineCss = require('inline-css'); // 将css嵌入行内
 const { join } = require('path');
@@ -25,14 +24,8 @@ let pollutionLevel = {
 };
 
 let post = async () => {
-  const timeStamp = await fetch(
-    'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-  )
-    .then((res) => res.json())
-    .then((json) => json.data.t);
+  let today = dayjs().add(8, 'hour');
 
-  let today = dayjs(+timeStamp).locale('zh-cn');
-  
   console.log(today.format());
 
   let lastDay = Math.floor(
--- test.js
+++ test.js
@@ -1,13 +1,6 @@
 const dayjs = require('dayjs');
-const fetch = require('node-fetch');
 
 (async () => {
-  const timeStamp = await fetch(
-    'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-  )
-    .then((res) => res.json())
-    .then((json) => json.data.t);
-
-  let today = dayjs(+timeStamp).locale('zh-cn');
+  let today = dayjs();
   console.log(today.format());
 })();
